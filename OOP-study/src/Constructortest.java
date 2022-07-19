ced4d240ac49317b8f59b0ed1aeed161395852a6
SJ.K <107601591+shinjaekim@users.noreply.github.com>
2022-07-19 23:12:08 +0900

    constructor correction

--- OOP-study/src/Constructortest.java
+++ OOP-study/src/Constructortest.java
@@ -11,6 +11,9 @@ public class Constructortest {
 
         Car myThirdCar = new Car("Perari");
         System.out.println(myThirdCar.color + myThirdCar.maker);
+
+        Car copyCar = new Car(myThirdCar);
+        System.out.println(copyCar.color + copyCar.maker); // 인스턴스가 생성자 호출로 복사된 것을 확인할 수 있다.
     }
 }
 class Car{
@@ -19,6 +22,11 @@ class Car{
     String color;
 
     public Car(){} // 기본생성자
+
+    public Car(Car c){      // 인스턴스를 복사한다.
+        maker = c.maker;
+        color = c.color;
+    }
 
     public Car(String maker){
         this(maker, "White");  //// 여기서 this 는 생성자를 뜻함. 괄호 안에 아무것도 없기 때문에 기본 생성자 호출 & 무조건 첫 줄
